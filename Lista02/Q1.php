47e097d3d3cd1edf4c9e81e5f849de15defbc82d
lana <lanatriforce1234@gmail.com>
2022-03-23 11:00:39 -0300

    Q1) Somar todos os números pares menores que 100

---
+++ Lista02/Q1.php
@@ -0,0 +1,17 @@
+<?php
+    //Lana Akemi Ihara
+    //BI3005771
+    //3º INFO
+
+    //Declaração das variáveis.
+    $num = 0;
+    $soma = 0;
+    
+    //Comando de repetição, para somar todos os números pares menores que 100.
+    for ($num; $num < 10; $num + 2)
+        $soma = $soma + $num;
+    echo "A soma de todos os números pares menores que 100 resulta em: ".$soma.".";
+    // http://localhost/prw/Lista02/Q1.php
+    // http://127.0.0.1/prw/Lista02/Q1.php
+
+?>
