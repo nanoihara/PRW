ebd231f736cde8e0c024b35920b117067b014930
nano <murilo.varges@gmail.com>
2022-03-30 10:43:58 -0300

    exemplo de manipulação de string na aula 6

---
+++ aula06/exstring.php
@@ -0,0 +1,9 @@
+<?php
+    $palavra = " Ciências da Computação ";
+    echo "A variável contém: ".strlen($palavra)."caracteres"."<br>";
+    $palavra = trim($palavra);
+    echo "A variável agora contém: ".strlen($palavra)."caracteres";
+
+    // http://localhost/prw/aula06/exstring.php
+    // http://127.0.0.1/prw/aula06/exstring.php
+?>
