da2db22a5a3b14f8499cc49a4d128d5c7e2295de
nano <lanatriforce1234@gmail.com>
2022-04-03 17:27:46 -0300

    1 - quantidade de caracteres em uma frase

--- Lista03/Q1.php
+++ Lista03/Q1.php
@@ -14,7 +14,7 @@
 
     //Declaração das variáveis.
     $frase = " torrada amantegada e suco  de laranja      doce ";
-    $fraseEspaco = "";
+    $fraseEspaco;
     
     //Impressão da palavra ou frase armazenada dentro da variável $frase
     echo "<br><br>A frase ou palavra digitada é: ".$frase;
@@ -22,8 +22,9 @@
     //Comando de manipulação de string para contar a quatidade de caracteres (strlen) da palavra ou frase atual.
     echo "<br>A quantidade de caracteres dela (contando os espaços), é de: ".strlen($frase);
 
-    //Comando de manipulação de string para retirar os espaços iniciais e finais (trim) e contar a quantidade de caracteres (strlen) da palavra ou frase.
+    //Comando de manipulação de string para retirar os espaços iniciais e finais (trim), tirar os demais espaços (str_replace) e contar a quantidade de caracteres (strlen) da palavra ou frase.
     $fraseEspaco = trim($frase);
+    $fraseEspaco = str_replace(" ", "", $frase);
 
     echo "<br><br>A quantidade de caracteres da frase *".$frase."* (sem contar os espaços iniciais e finais), é de: ".strlen($fraseEspaco);
 
