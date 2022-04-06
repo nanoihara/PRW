21056528583e4dcdb57b23ebc7777a0045b49133
nano <lanatriforce1234@gmail.com>
2022-04-06 09:52:25 -0300

    sim

--- aula06/cadastro/cadastro.php
+++ aula06/cadastro/cadastro.php
@@ -5,4 +5,7 @@
 
    echo 'Nome: ' . $nome . '<br>';
    echo 'E-mail: ' . $email . '<br>';
+
+    // http://localhost/prw/aula06/cadastro/cadastro.php
+    // http://127.0.0.1/prw/aula06/cadastro.php
 ?>
