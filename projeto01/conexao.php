fcd960bbef724b99039f2814ce6c5faf4ddb4552
lana <lanatriforce1234@gmail.com>
2022-04-20 11:04:31 -0300

    conexão do banco de dados com o site projeto01

--- projeto01/conexao.php
+++ projeto01/conexao.php
@@ -6,10 +6,10 @@
     $password = "";
     $database = "projeto01";
     $con = mysqli_connect($hostname, $username, $password, $database, $port);
-    if (mysqli_connect_errno())
+    if (mysqli_connect_errno()) {
         printf("Erro ao conectar ao banco de dados: %s\n", mysqli_connect_error());
         exit;
-
+    }
     printf("Banco de dados conectado com sucesso \o/");
 
     // http://localhost/prw/projeto01/conexao.php
