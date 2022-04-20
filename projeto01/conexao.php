338be10a25981585896b0a4440e97c09c5f88f2f
lana <lanatriforce1234@gmail.com>
2022-04-20 10:55:33 -0300

    conexão entre o banco de dados e o site projeto01

--- projeto01/conexao.php
+++ projeto01/conexao.php
@@ -9,6 +9,9 @@
     if (mysqli_connect_errno())
         printf("Erro ao conectar ao banco de dados: %\n", mysqli_connect_error());
         exit;
-        
+
     printf("Banco de dados conectado com sucesso \o/");
+
+    // http://localhost/prw/projeto01/conexao.php
+    // http://127.0.0.1/prw/projeto01/conexao.php
 ?>
