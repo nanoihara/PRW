209bcc57f0e747426801fa25f371777877c1636a
nano <lanatriforce1234@gmail.com>
2022-04-20 09:15:08 -0300

    exemplo aula - formulário e um banco de dados

---
+++ projeto01/cadastro_usuario.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Exemplo de Aula</title>
+</head>
+<body>
+<?php
+    // LANA AKEMI IHARA - 3º INFO
+    /* Ex: Criar um script php chamado “cadastro_usuario.php” que receberá as
+    informações do formulário. */
+
+    //Impressão do nome.
+    echo "Exemplo de Aula - Formulário associado a um banco de dados (cadastro de um usuário),<br>feito por Lana Akemi Ihara, 3º INFO.";
+
+    //Declaração das variáveis.
+    $nome_usuario = $_POST["nome_usuario"];
+    $email_usuario = $_POST["email_usuario"];
+    $fone_usuario = $_POST["fone_usuario"];
+
+    //Impressão dos dados recebidos do formulário.
+    echo "<p><b>Nome do usuario:</b> ".$nome_usuario."<br>";
+    echo "<b>E-mail:</b> ".$email_usuario."<br>";
+    echo "<b>Telefone:</b> ".$fone_usuario."</p>";
+    // http://localhost/prw/projeto01/cadastro_usuario.htm
+    // http://127.0.0.1/prw/projeto01/cadastro_usuario.htm
+
+?>
